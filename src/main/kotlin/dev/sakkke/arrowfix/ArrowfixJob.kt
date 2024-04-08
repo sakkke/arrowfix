1512ad7960db660451cd97238906a389fa010a6b
sakkke <84666033+sakkke@users.noreply.github.com>
2024-04-08 13:48:22 +0900

    feat: add snowball to targets

--- src/main/kotlin/dev/sakkke/arrowfix/ArrowfixJob.kt
+++ src/main/kotlin/dev/sakkke/arrowfix/ArrowfixJob.kt
@@ -7,8 +7,9 @@ import org.quartz.JobExecutionContext
 class ArrowfixJob: Job {
     override fun execute(p0: JobExecutionContext?) {
         Bukkit.getScheduler().runTask(Arrowfix.instance, Runnable {
-            Arrowfix.instance.logger.info("Killing all arrows")
+            Arrowfix.instance.logger.info("Killing all the arrow-related entities")
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kill @e[type=arrow]")
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kill @e[type=snowball]")
         })
     }
 }
